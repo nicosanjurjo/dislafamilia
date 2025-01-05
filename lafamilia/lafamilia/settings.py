0e76e9901f23151cfb13825c1042456f9c1355f4
nicosanjurjo <saanjur@gmail.com>
2025-01-05 02:56:40 -0300

    Last commit debug off

--- lafamilia/lafamilia/settings.py
+++ lafamilia/lafamilia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^1bai#4agb2_h=i#eehju8*$z0xy_0m9yx!svogqp0trj3od4d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['217.196.62.73', 'www.distrilafamilia.site', 'distrilafamilia.site', '192.168.1.102', '127.0.0.1', '192.168.1.103']
 
